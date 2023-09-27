4b4e186c8f5ed6086d8cbba5249584d72046a6a3
janeesh27 <janeesh.tin@gmail.com>
2023-09-27 06:27:09 +0530

    work page and sanity studio added

--- portfolio-2/src/components/Work.jsx
+++ portfolio-2/src/components/Work.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const Work = () => {
   return (
     <>
-      <div>work</div>
+      <div className="transform translate-y-[7vh] mx-[1rem] sm:mx-[10rem] text-center">
+        <div className="text-[#C5C6C7] font-semibold text-[2rem]">Projects</div>
+        <div className="text-[#C5C6C7] text-[1rem] sm:text-[1.5rem] pt-4">
+          Welcome to my Projects Showcase, where I demonstrate my proficiency in
+          creating engaging and user-friendly web experiences. These projects
+          exemplify my expertise in crafting seamless interfaces with a focus on
+          user-centric design principles. Each project below showcases my
+          ability to implement responsive layouts, optimize performance, and
+          deliver an exceptional user experience.
+        </div>
+      </div>
     </>
   );
 };
