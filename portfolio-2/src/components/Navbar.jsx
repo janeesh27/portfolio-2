cc9539fbf5fd3039db5c61278d6e269c5d12057e
janeesh27 <janeesh.tin@gmail.com>
2023-10-05 11:15:19 +0530

    ADDED routes

--- portfolio-2/src/components/Navbar.jsx
+++ portfolio-2/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="grid grid-cols-[1fr_1fr] text-[#C5C6C7] font-semibold md:grid-cols-[6fr_1fr] lg:grid-cols-[5fr_1fr] text-[1rem] md:text-[1.5rem] pt-[1rem] mx-[1rem]">
-      <Link href="/" className="hover:text-[#66FCF1]">
+      <Link href="/" className="hover:text-[#66FCF1] transition-[2s]">
         Janeesh
       </Link>
       <div className="col-start-2 grid grid-cols-2 gap-x-4">
