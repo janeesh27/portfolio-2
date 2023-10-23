b3da736e6583a137cdb67b37c3a7ee7b157143db
janeesh27 <janeesh.tin@gmail.com>
2023-10-23 10:22:35 +0530

    NPM packages update

--- portfolio-2/src/app/page.js
+++ portfolio-2/src/app/page.js
@@ -1,7 +1,5 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import About from "@/app/about/page";
-import Work from "@/app/work/page";
 
 export default function Home() {
   return (
