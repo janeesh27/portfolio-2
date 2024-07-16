212d9a4f50a2cb4ca3e2e13cfed5779c2f4a9801
janeesh27 <janeesh.tin@gmail.com>
2024-07-16 23:01:36 +0530

    Hero Component Fix

--- portfolio-2/src/components/Hero.jsx
+++ portfolio-2/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <>
       <div className="text-center mx-auto p-4 mt-[170px]">
-        <div className="text-[1.5rem] md:text-[2rem] text-[#66FCF1]">
+        <div className="text-[1.5rem] md:text-[2rem] min-h-[40px] text-[#66FCF1]">
           <TypingElement message="Heello There..." />
         </div>
         <p className="text-[1.25rem] md:text-[1.5rem] text-[#C5C6C7]">
@@ -18,17 +18,20 @@ const Hero = () => {
         <div className="grid grid-flow-col sm:w-[30%] sm:mx-auto ">
           <div className="mx-auto">
             <a href="https://www.linkedin.com/in/janeeshpalsingh">
-              <FaLinkedinIn className="w-4 h-4" fill="#66FCF1" />
+              <FaLinkedinIn
+                className="w-4 h-4 hover:scale-110"
+                fill="#66FCF1"
+              />
             </a>
           </div>
           <div className="mx-auto">
             <a href="https://github.com/janeesh27">
-              <FaGithub className="w-4 h-4" fill="#66FCF1" />
+              <FaGithub className="w-4 h-4 hover:scale-110" fill="#66FCF1" />
             </a>
           </div>
           <div className="mx-auto">
             <a href="https://twitter.com/_janeesh_">
-              <FaTwitter className="w-4 h-4" fill="#66FCF1" />
+              <FaTwitter className="w-4 h-4 hover:scale-110" fill="#66FCF1" />
             </a>
           </div>
         </div>
